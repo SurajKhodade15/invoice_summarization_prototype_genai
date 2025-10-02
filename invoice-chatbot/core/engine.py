637f4a0025503d04b3627447716a4ed1c99f13c1
Suraj Khodade <suraj.khodade7@gmail.com>
2025-10-02 15:16:34 +0530

    Fixed errors

--- invoice-chatbot/core/engine.py
+++ invoice-chatbot/core/engine.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
